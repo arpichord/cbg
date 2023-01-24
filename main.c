46b7125653b839ba3dbaf3533c995dc696b2758a
arpichord <t@arpichord.com>
2023-01-23 22:05:49 -0500

    Building display to help visualize.

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "board.h"
+#include "piece.h"
+#include "display.h"
+#include "game.h"
 
 int main(int argc, char*argv[]){
+
+    // Display board
+    Board* b;
+    setup_board(&b, 8, 8);
     
+    display(b, DISPLAY_MODE_CLI);
+    
+
+    
+
+
+
 }
