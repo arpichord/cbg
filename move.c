98682c3415db68b6ee8496ac00492bb4061f58bd
arpichord <t@arpichord.com>
2023-02-17 22:01:34 -0500

    Accidental header guards in .c

--- move.c
+++ move.c
@@ -1,12 +1,9 @@
-#ifndef H_MOVE
-#define H_MOVE
-
 #include "board.h"
 #include "piece.h"
 
 int move_piece(Board* board, Piece* piece) {
 
-    
+
 
 };
 
@@ -21,5 +18,3 @@ int _update_valid_moves(Board* board, Piece* piece) {
 int _update_position_association(Board* board, Piece* piece) {
 
 };
-
-#endif
